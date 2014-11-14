7868e9e82153104946858f3f1136fa4c5b6ce27d
jeremija <jerko.steiner@gmail.com>
2014-11-14 20:24:56 +0100

    Ignoring test/setup/tests.js as it is automatically generated

--- test/setup/tests.js
+++
@@ -1,9 +0,0 @@
-// auto generated unit tests lists based on file extension
-// do not commit this file!
-var tests = [
-    'js/component-test.js',
-    'js/defineBinding-test.js',
-    'js/existingBindingHandlers-test.js',
-    'js/failures-test.js',
-    'js/nestedComponents-test.js',
-];
